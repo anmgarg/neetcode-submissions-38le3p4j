eba5d21cf7d014932ef13ffacc95d5495bbc4393
Anmol Garg <34094069+anmgarg@users.noreply.github.com>
2026-04-16 22:30:04 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int i = 0;
+
+        for (int j=1; j<prices.length; j++) {
+            if(prices[j] > prices[i]) {
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+            } else {
+                i = j;
+            }
+        }
+        return maxProfit;
+    }
+}
